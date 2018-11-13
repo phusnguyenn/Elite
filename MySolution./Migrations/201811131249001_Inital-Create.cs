55798460f86ca153f6dd8248f353741a2d0f3306
phusnguyenn <phunguyenn1602@gmail.com>
2018-11-13 23:01:07 +0700

    add product detail page

---
+++ MySolution./Migrations/201811131249001_Inital-Create.cs
@@ -0,0 +1,150 @@
+namespace Elite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitalCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Category",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        ParentId = c.Long(),
+                        CategoryName = c.String(),
+                        Description = c.String(),
+                        MainImage = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreatorUserId = c.Long(),
+                        CreationTime = c.DateTime(nullable: false),
+                        DeleteUserId = c.Long(),
+                        DeletedTime = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Category", t => t.ParentId)
+                .Index(t => t.ParentId);
+            
+            CreateTable(
+                "dbo.OrderDetail",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        OrderID = c.Long(nullable: false),
+                        ProductId = c.Long(nullable: false),
+                        Quanlity = c.Int(nullable: false),
+                        TotalAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreatorUserId = c.Long(),
+                        CreationTime = c.DateTime(nullable: false),
+                        DeleteUserId = c.Long(),
+                        DeletedTime = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Order", t => t.OrderID, cascadeDelete: true)
+                .ForeignKey("dbo.Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.OrderID)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Order",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Message = c.String(),
+                        TotalAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        TotalProduct = c.Int(nullable: false),
+                        TotalQuanlity = c.Int(nullable: false),
+                        Description = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreatorUserId = c.Long(),
+                        CreationTime = c.DateTime(nullable: false),
+                        DeleteUserId = c.Long(),
+                        DeletedTime = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Product",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        ProductName = c.String(),
+                        CategoryId = c.Long(nullable: false),
+                        Gender = c.Byte(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        OldPrice = c.Decimal(precision: 18, scale: 2),
+                        Description = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreatorUserId = c.Long(),
+                        CreationTime = c.DateTime(nullable: false),
+                        DeleteUserId = c.Long(),
+                        DeletedTime = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Category", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Role",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        RoleName = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreatorUserId = c.Long(),
+                        CreationTime = c.DateTime(nullable: false),
+                        DeleteUserId = c.Long(),
+                        DeletedTime = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.User",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Username = c.String(),
+                        Password = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Address = c.String(),
+                        PhoneNumber = c.String(),
+                        Email = c.String(),
+                        Gender = c.Byte(nullable: false),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        RoleId = c.Long(nullable: false),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreatorUserId = c.Long(),
+                        CreationTime = c.DateTime(nullable: false),
+                        DeleteUserId = c.Long(),
+                        DeletedTime = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Role", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.RoleId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.User", "RoleId", "dbo.Role");
+            DropForeignKey("dbo.OrderDetail", "ProductId", "dbo.Product");
+            DropForeignKey("dbo.Product", "CategoryId", "dbo.Category");
+            DropForeignKey("dbo.OrderDetail", "OrderID", "dbo.Order");
+            DropForeignKey("dbo.Category", "ParentId", "dbo.Category");
+            DropIndex("dbo.User", new[] { "RoleId" });
+            DropIndex("dbo.Product", new[] { "CategoryId" });
+            DropIndex("dbo.OrderDetail", new[] { "ProductId" });
+            DropIndex("dbo.OrderDetail", new[] { "OrderID" });
+            DropIndex("dbo.Category", new[] { "ParentId" });
+            DropTable("dbo.User");
+            DropTable("dbo.Role");
+            DropTable("dbo.Product");
+            DropTable("dbo.Order");
+            DropTable("dbo.OrderDetail");
+            DropTable("dbo.Category");
+        }
+    }
+}
